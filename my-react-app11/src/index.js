83954141bcc07fa68decafbdfae9ee5f663c2bee
VISHALSP969 <vishalsp969@gmail.com>
2024-09-11 18:03:19 +0530

    One top level element

---
+++ my-react-app11/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const myElement=(
+  <div>
+    <p>I am a paragraph.</p>
+    <p>I am a paragraph too.</p>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  myElement
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
