9dfcb0b4d6262ca953a7dd2594b67b44c14640d0
VISHALSP969 <vishalsp969@gmail.com>
2024-09-12 04:05:54 +0530

    Class constructor

---
+++ class-component1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+class Car extends React.Component{
+  constructor(){
+    super();
+    this.state={color:"red"}
+  }
+  render(){
+    return <h1>Hi, I am a {this.state.color} Car!</h1>
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Car />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
