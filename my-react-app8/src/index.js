ee07f3a50dd92b7f2b4567832a461b7ac62df03c
VISHALSP969 <vishalsp969@gmail.com>
2024-09-11 16:16:31 +0530

    Create React element without JSX

---
+++ my-react-app8/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const myElement=React.createElement('h1',{},'I do not use JSX!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  myElement
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
