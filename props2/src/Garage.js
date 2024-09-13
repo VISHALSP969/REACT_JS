7ec7e555c0ada26e0d9d7fbddb959fc3312ee5be
VISHALSP969 <vishalsp969@gmail.com>
2024-09-13 20:59:57 +0530

    React props

---
+++ props2/src/Garage.js
@@ -0,0 +1,12 @@
+import Car from "./Car";
+
+function Garage(){
+    return(
+        <div>
+            <h1>Who lives in my Garage?</h1>
+            <Car brand="Ford"/>
+        </div>
+    );
+}
+
+export default Garage;
