ebe1c343db86cbf6aa4c59372f3103710ad564df
VISHALSP969 <vishalsp969@gmail.com>
2024-09-13 22:00:57 +0530

    React events

---
+++ events1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+function Football(){
+  const shoot=()=>{
+    alert("Great Shot!");
+  }
+  return(
+    <button type="button" onClick={shoot}>Take the shot!</button>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Football/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
