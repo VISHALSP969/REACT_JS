e45fa90997b4a31031ead8656a5567d657bacfa7
VISHALSP969 <vishalsp969@gmail.com>
2024-09-13 19:47:26 +0530

    Unmounting

---
+++ life-cycle10/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+class Container extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={show:true};
+  }
+  delHeader=()=>{
+    this.setState({show:false});
+  }
+  render(){
+    let myheader;
+    if(this.state.show){
+      myheader=<Child/>;
+    }
+    return(
+      <div>
+        {myheader}
+        <button type="button" onClick={this.delHeader}>Delete Header</button>
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component{
+  componentWillUnmount(){
+    alert("The component named Header is about to be unmounted.");
+  }
+  render(){
+    return <h1>Hello World!</h1>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Container/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
