f1ec0654feb4cc3d17f7d34eb6bbd3bc6226ea00
VISHALSP969 <vishalsp969@gmail.com>
2024-09-14 15:12:58 +0530

    Handling Forms

---
+++ react-forms2/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {useState} from 'react';
+
+function MyForm(){
+  const [name,setName]=useState("");
+  return(
+    <form>
+      <label>Enter your name : 
+        <input 
+        type="text" 
+        value={name} 
+        onChange={(e)=>setName(e.target.value)}/>
+      </label>
+    </form>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MyForm/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
