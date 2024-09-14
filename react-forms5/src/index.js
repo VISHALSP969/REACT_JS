a083ced74c61a52e5c22ab7da3c97b6b195d2da7
VISHALSP969 <vishalsp969@gmail.com>
2024-09-14 19:03:14 +0530

    Multiple input fields

---
+++ react-forms5/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {useState} from 'react';
+
+function MyForm(){
+  //initialize the state variables for form inputs(name and age)
+  const [name,setName]=useState("");
+  const [age,setAge]=useState("");
+
+  //Handle form submission
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    alert(`Name:${name},Age:${age}`);
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name :
+        <input
+          type="text"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        />
+      </label><br/>
+      <label>Enter your age : 
+        <input
+          type="number"
+          value={age}
+          onChange={(e)=> setAge(e.target.value)}
+        />
+      </label><br/>
+      <button type="submit">SUBMIT</button>
+    </form>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MyForm/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
