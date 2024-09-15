e3c68a7e5413467d30f8b7cb7157a4b7c7bdb829
VISHALSP969 <vishalsp969@gmail.com>
2024-09-15 23:58:36 +0530

    Sass styling

---
+++ react-styling4/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import './my-sass.scss';
+
+const Header=()=>{
+  return(
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Header/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
