2698ee1c29dac3804a226bb7ed56e68edc3d0e39
VISHALSP969 <vishalsp969@gmail.com>
2024-09-16 11:54:09 +0530

    useEffect Hook

---
+++ hooks8/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {useState,useEffect} from 'react';
+
+function Timer(){
+  const [count,setCount]=useState(0);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount((count)=> count+1);
+    },1000);
+  },[]);    //add a empty bracket here
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Timer/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
