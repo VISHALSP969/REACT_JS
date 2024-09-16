f5df049f16dfff00f17d2698a4108b7d3e1b570e
VISHALSP969 <vishalsp969@gmail.com>
2024-09-16 10:16:37 +0530

    useState Hook

---
+++ hooks6/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {useState} from 'react';
+
+function Car(){
+  const [car,setCar]=useState({
+    brand:"Ford",
+    model:"Mustang",
+    year:1964,
+    color:"red"
+  });
+
+  const updateColor=()=>{
+    setCar(previousState=>{
+      return {...previousState,color:"blue"}
+    });
+  }
+  return(
+    <>
+      <h1>My {car.brand}</h1>
+      <p>It is a {car.color} {car.model} from {car.year}.</p>
+      <button
+        type="button"
+        onClick={updateColor}
+      >Blue</button>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Car/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
