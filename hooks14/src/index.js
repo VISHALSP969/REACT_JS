d1b88811cdda7ab0b6d9ee50d424cb64c362532f
VISHALSP969 <vishalsp969@gmail.com>
2024-09-16 16:44:40 +0530

    useRef Hook

---
+++ hooks14/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {useRef} from 'react';
+
+function App(){
+ const inputElement=useRef();
+ const focusInput=()=>{
+  inputElement.current.focus();
+ };
+
+ return(
+  <>
+    <input type="text" ref={inputElement}/>
+    <button onClick={focusInput}>Focus Input</button>
+  </>
+ );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
