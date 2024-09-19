c7274b06a76bc5a3e2a5ce4ea8995649a772bc03
VISHALSP969 <vishalsp969@gmail.com>
2024-09-19 16:04:17 +0530

    Calculator third commit

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -36,25 +36,25 @@ function Calculator() {
         <button onClick={() => handleClick("7")}>7</button>
         <button onClick={() => handleClick("8")}>8</button>
         <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("/")} className="operator">/</button>
         <br/>
 
         <button onClick={() => handleClick("4")}>4</button>
         <button onClick={() => handleClick("5")}>5</button>
         <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("*")}>*</button>
+        <button onClick={() => handleClick("*")} className="operator">*</button>
         <br/>
 
         <button onClick={() => handleClick("1")}>1</button>
         <button onClick={() => handleClick("2")}>2</button>
         <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick("-")} className="operator">-</button>
         <br/>
 
         <button onClick={() => handleClick(".")}>.</button>
         <button onClick={() => handleClick("0")}>0</button>
         <button onClick={calculate} className="equal">=</button>
-        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("+")} className="operator">+</button>
 
       </div>
     </div>
